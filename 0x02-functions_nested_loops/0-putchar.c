a247f8616e65f139078f1a1d06bf717c5f902932
charity Mugo <charitymugo11@gmail.com>
2022-03-15 15:08:12 -0400

    changes

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
 #include "main.h"
-
 /**
- * main -  prints -putchar, followed by a new line
+ * main - Prints _putchar
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	_putchar('-');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('e');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+        _putchar('_');
+        _putchar('p');
+        _putchar('u');
+        _putchar('t');
+        _putchar('c');
+        _putchar('h');
+        _putchar('a');
+        _putchar('r');
+        _putchar('\n');
+        return (0);
 }
